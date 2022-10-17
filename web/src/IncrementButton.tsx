31a109503a444de80cd0d8c3b6d0c8962387ad7f
S4-K1 <hardcastle.riley@gmail.com>
2022-10-17 20:18:42 +0100

    Now working :)

--- web/src/IncrementButton.tsx
+++ web/src/IncrementButton.tsx
@@ -1,20 +1,19 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 type ComponentProps = {
   incrementBy: number,
 }
 
 function IncrementButton(props: ComponentProps) {
-
   const [count, setCount] = useState(0);
 
-    return (
-      <div>
-        <button onClick={() => setCount((count) => count + props.incrementBy)}>
-            This button increments by {props.incrementBy}. 
+  return (
+    <div>
+      <button onClick={() => setCount(() => count + props.incrementBy)}>
+            This button increments by {props.incrementBy}.
             count is {count}
-          </button>
-      </div>
+      </button>
+    </div>
   );
 }
 
