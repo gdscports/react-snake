6246660c91b7601ec52631ca7d928fbec3f406db
S4-K1 <hardcastle.riley@gmail.com>
2022-10-19 17:57:58 +0100

    Setting up state and conditional rendering

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,30 +1,41 @@
 import './App.css';
 
+import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import IncrementButton from './IncrementButton';
 
 function App() {
-  return (
-    <div className='App'>
+  const [hasStarted, setHasStarted] = useState(false);
+
+  if (!hasStarted) {
+    return (
       <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+        <h1>hi</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <IncrementButton incrementBy={1} />
-        <IncrementButton incrementBy={2} />
-        <p>
+    );
+  } else {
+    return (
+      <div className='App'>
+        <div>
+          <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
+            <img src='/vite.svg' className='logo' alt='Vite logo' />
+          </a>
+          <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
+            <img src={reactLogo} className='logo react' alt='React logo' />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <IncrementButton incrementBy={1} />
+          <IncrementButton incrementBy={2} />
+          <p>
           Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+          </p>
+        </div>
+        <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
       </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
