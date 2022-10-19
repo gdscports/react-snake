eb71e8ff6a505a884974466f61c1a3006b2385ca
Jack Moseley <jackwmoseley@gmail.com>
2022-10-19 18:02:32 +0100

    Formatted & linted files

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,30 +1,30 @@
 import './App.css';
 
-import reactLogo from './assets/react.svg';
 import IncrementButton from './IncrementButton';
+import reactLogo from './assets/react.svg';
 
 function App() {
-  return (
-    <div className='App'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <IncrementButton incrementBy={1} />
-        <IncrementButton incrementBy={2} />
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div>
+				<a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
+					<img src='/vite.svg' className='logo' alt='Vite logo' />
+				</a>
+				<a href='https://reactjs.org' target='_blank' rel='noreferrer'>
+					<img src={reactLogo} className='logo react' alt='React logo' />
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className='card'>
+				<IncrementButton incrementBy={1} />
+				<IncrementButton incrementBy={2} />
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+		</div>
+	);
 }
 
 export default App;
