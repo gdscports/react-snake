91fa84a691e4f669ee5f809240044aa66171b9c6
Mark Chittenden <41925735+mark-chit@users.noreply.github.com>
2022-10-19 17:50:36 +0100

    Added board and snake 🐍

--- web/src/SnakeEngine/SnakeEngine.ts
+++ web/src/SnakeEngine/SnakeEngine.ts
@@ -1,6 +1,17 @@
-export class SnakeEngine{
+export class SnakeEngine {
+  static board = [];
+  static snake = {
+    length: 3,
+    body: [
+      { x: 0, y: 0 },
+      { x: 1, y: 0 },
+      { x: 2, y: 0 },
+    ],
 
-    static helloWorld(){
-      console.log("Hello World")
-    }
+    direction: 'right',
+  };
+
+  static helloWorld() {
+    console.log('Hello World');
   }
+}
