f61f1629bc30688d90c3e245c42ab90d029a4efb
Johnny Morrison-Howe <50030862+Dexyboiiii@users.noreply.github.com>
2022-10-26 17:34:03 +0100

    Moved SplashScreen and SnakeScreen into separate files

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,39 +1,8 @@
 import './App.css';
 
 import { useState, Dispatch } from 'react';
-import IncrementButton from './IncrementButton';
-import reactLogo from './assets/react.svg';
-
-function SplashScreen(props: {setHasStarted: Dispatch<boolean>}) {
-  return (
-      <div>
-        <h1>Welcome to React Snake UwU</h1>
-        <button onClick={() => props.setHasStarted(true)}>
-          Click me to start the game
-        </button>
-      </div>
-    );
-}
-
-function SnakeScreen() {
-  return (
-      <div className='App'>
-        <div>
-          <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-            <img src='/vite.svg' className='logo' alt='Vite logo' />
-          </a>
-          <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-            <img src={reactLogo} className='logo react' alt='React logo' />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <IncrementButton incrementBy={1} />
-          <IncrementButton incrementBy={2} />
-        </div>
-      </div>
-    );
-}
+import SnakeScreen from './components/SnakeScreen';
+import SplashScreen from './components/SplashScreen';
 
 function App() {
   const [hasStarted, setHasStarted] = useState(false);
