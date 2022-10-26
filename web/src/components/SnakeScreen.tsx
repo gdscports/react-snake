f6264aa3257dcac8afca197b304413a616a24acf
Johnny Morrison-Howe <50030862+Dexyboiiii@users.noreply.github.com>
2022-10-26 17:54:50 +0100

    Changed splash screen to be a snake
    sssssssssssssssssssss

--- web/src/components/SnakeScreen.tsx
+++ web/src/components/SnakeScreen.tsx
@@ -1,18 +1,10 @@
 import IncrementButton from '../IncrementButton';
-import reactLogo from '../assets/react.svg';
 
 function SnakeScreen() {
     return (
         <div className='App'>
-          <div>
-            <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-              <img src='/vite.svg' className='logo' alt='Vite logo' />
-            </a>
-            <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-              <img src={reactLogo} className='logo react' alt='React logo' />
-            </a>
-          </div>
-          <h1>Vite + React</h1>
+          <div className='logo snake'>🐍</div>
+          <h1>GDSC Snake</h1>
           <div className="card">
             <IncrementButton incrementBy={1} />
             <IncrementButton incrementBy={2} />
