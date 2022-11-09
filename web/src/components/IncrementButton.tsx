111f904529572c95a6fe55540da3d10dba2e7820
S4-K1 <hardcastle.riley@gmail.com>
2022-11-09 17:00:01 +0000

    Remove excess

--- web/src/components/IncrementButton.tsx
+++ web/src/components/IncrementButton.tsx
@@ -1,14 +1,11 @@
-import { useState, Dispatch } from 'react';
+import { Dispatch } from 'react';
 
-type ComponentProps = {
-	incrementBy: number;
-};
 
 function IncrementButton(props: {countIncrease: Dispatch<number>, incrementBy: number}) {
 	return (
 		<div>
-			<button onClick={() => props.countIncrease(props.props.incrementBy)}>
-				This button increments by {props.props.incrementBy}
+			<button onClick={() => props.countIncrease(props.incrementBy)}>
+				This button increments by {props.incrementBy}
 			</button>
 		</div>
 	);
