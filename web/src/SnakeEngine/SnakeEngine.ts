d816c44a53c9f7fe353305b4a9d8d43b93e4ff5b
Jonas Oppong <114658990+Jonas-Oppong@users.noreply.github.com>
2022-11-23 17:59:54 +0000

    fixed indentation and added board size

--- web/src/SnakeEngine/SnakeEngine.ts
+++ web/src/SnakeEngine/SnakeEngine.ts
@@ -14,69 +14,71 @@ export class SnakeEngine {
       }
   }
 
-    static snake = {
-        body: [
-          { x: 0, y: 0 },
-          { x: 1, y: 0 },
-          { x: 2, y: 0 },
-        ],
-        direction: 'right',
-      };
+  static boardSize = 9;
 
-      static model() {
-        let x = SnakeEngine.snake.body[SnakeEngine.snake.body.length - 1].x;
-        let y = SnakeEngine.snake.body[SnakeEngine.snake.body.length - 1].y;
-        switch (SnakeEngine.snake.direction) {
-          case 'up':
-            y = y - 1;
-            if (y < 0) {
-              y = 9;
-            }
-            break;
-          case 'down':
-            y = y + 1;
-            if (y > 9) {
-              y = 0;
-            }
-            break;
-          case 'left':
-            x = x - 1;
-            if (x < 0) {
-              x = 9;
-            }
-            break;
-          case 'right':
-            x = x + 1;
-            if (x > 9) {
-              x = 0;
-            }
-            break;
-          default:
-        }
-        console.log('Model x; ', x, 'Model y: ', y);
-        const newSnakeHead = { x: x, y: y };
-        SnakeEngine.snake.body.shift();
-        SnakeEngine.snake.body.push(newSnakeHead);
+  static snake = {
+      body: [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 2, y: 0 },
+      ],
+      direction: 'right',
+    };
+
+    static model() {
+      let x = SnakeEngine.snake.body[SnakeEngine.snake.body.length - 1].x;
+      let y = SnakeEngine.snake.body[SnakeEngine.snake.body.length - 1].y;
+      switch (SnakeEngine.snake.direction) {
+        case 'up':
+          y = y - 1;
+          if (y < 0) {
+            y = SnakeEngine.boardSize;
+          }
+          break;
+        case 'down':
+          y = y + 1;
+          if (y > SnakeEngine.boardSize) {
+            y = 0;
+          }
+          break;
+        case 'left':
+          x = x - 1;
+          if (x < 0) {
+            x = SnakeEngine.boardSize;
+          }
+          break;
+        case 'right':
+          x = x + 1;
+          if (x > SnakeEngine.boardSize) {
+            x = 0;
+          }
+          break;
+        default:
       }
+      console.log('Model x; ', x, 'Model y: ', y);
+      const newSnakeHead = { x: x, y: y };
+      SnakeEngine.snake.body.shift();
+      SnakeEngine.snake.body.push(newSnakeHead);
+    }
 
-    static controller() {
-      document.addEventListener('keydown', parseInput);
-      function parseInput(e: KeyboardEvent) {
-        switch (e.key) {
-          case 'w':
-            SnakeEngine.snake.direction = 'up';
-            break;
-          case 'a':
-            SnakeEngine.snake.direction = 'left';
-            break;
-          case 's':
-            SnakeEngine.snake.direction = 'down';
-            break;
-          case 'd':
-            SnakeEngine.snake.direction = 'right';
-            break;
-          default:
-        }
+  static controller() {
+    document.addEventListener('keydown', parseInput);
+    function parseInput(e: KeyboardEvent) {
+      switch (e.key) {
+        case 'w':
+          SnakeEngine.snake.direction = 'up';
+          break;
+        case 'a':
+          SnakeEngine.snake.direction = 'left';
+          break;
+        case 's':
+          SnakeEngine.snake.direction = 'down';
+          break;
+        case 'd':
+          SnakeEngine.snake.direction = 'right';
+          break;
+        default:
       }
     }
+  }
 }
