e90e7bc82b40483902c5a6c09bcc318212b2762b
JakeIV <114679283+JakeIV@users.noreply.github.com>
2022-11-23 16:48:43 +0000

    Snake Created

--- web/src/SnakeEngine/SnakeEngine.ts
+++ web/src/SnakeEngine/SnakeEngine.ts
@@ -2,4 +2,13 @@ export class SnakeEngine {
     static helloWorld() {
         console.log('Hello World');
     }
+
+    static snake = {
+        body: [
+          { x: 0, y: 0 },
+          { x: 1, y: 0 },
+          { x: 2, y: 0 },
+        ],
+        direction: 'right',
+      };
 }
