b64d9a1667001016fab8795eebd05fbd5fd5a28c
Jonas Oppong <114658990+Jonas-Oppong@users.noreply.github.com>
2022-11-23 17:01:31 +0000

    Added Controller

--- web/src/SnakeEngine/SnakeEngine.ts
+++ web/src/SnakeEngine/SnakeEngine.ts
@@ -11,4 +11,25 @@ export class SnakeEngine {
         ],
         direction: 'right',
       };
+
+    static controller() {
+      document.addEventListener('keydown', parseInput);
+      function parseInput(e: { key: string}) {
+        switch (e.key) {
+          case 'w':
+            SnakeEngine.snake.direction = 'up';
+            break;
+          case 'a':
+            SnakeEngine.snake.direction = 'left';
+            break;
+          case 's':
+            SnakeEngine.snake.direction = 'down';
+            break;
+          case 'd':
+            SnakeEngine.snake.direction = 'right';
+            break;
+          default:
+        }
+      }
+    }
 }
